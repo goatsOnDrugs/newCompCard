c92ae4487ab5138c681d5987fe91ac2470bb197a
Joey Ricketts <joeyricketts@spires-MBP.hsd1.co.comcast.net>
2019-08-03 13:16:44 -0600

    added styled to route page and added a back button

--- src/pages/Route.js
+++ src/pages/Route.js
@@ -3,6 +3,12 @@ import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import QRCode from "qrcode.react";
 import styled from "styled-components";
+import {
+  IoIosAddCircleOutline,
+  IoIosRemoveCircleOutline,
+  IoMdRadioButtonOff,
+  IoMdCheckmarkCircle
+} from "react-icons/io";
 
 import Button from "../components/common/Button";
 import "./Route.css";
@@ -42,46 +48,73 @@ const Route = props => {
             if (loading) return "Loading...";
             if (error) return `Error! ${error.message}`;
             return (
-              <div>
+              <InnerCardWrapper>
+                <Back onClick={() => window.history.back()}>back</Back>
                 <h2>{data.route.title}</h2>
                 <h3>POINTS: {data.route.points}</h3>
                 <QRCode value={data.route.id} />
-                <div className='attempts-container'>
+                <AttemptsContainer className="attempts-container">
                   <h3>ATTEMPTS:</h3>
-                  {data.route.attempts > 0 ? (
+                  <CounterContainer>
+                    {data.route.attempts > 0 ? (
+                      <Mutation
+                        mutation={UPDATE_ATTEMPT_MUTATION}
+                        variables={{
+                          id: data.route.id,
+                          attempts: data.route.attempts - 1
+                        }}
+                      >
+                        {mutation => (
+                          <IoIosRemoveCircleOutline
+                            onClick={mutation}
+                            size="65px"
+                          />
+                        )}
+                      </Mutation>
+                    ) : (
+                      <IoIosRemoveCircleOutline size="65px" />
+                    )}
+                    <Attempts>{data.route.attempts}</Attempts>
                     <Mutation
                       mutation={UPDATE_ATTEMPT_MUTATION}
                       variables={{
                         id: data.route.id,
-                        attempts: data.route.attempts - 1
+                        attempts: data.route.attempts + 1
                       }}
                     >
-                      {mutation => <div onClick={mutation}>-</div>}
+                      {mutation => (
+                        <IoIosAddCircleOutline onClick={mutation} size="65px" />
+                      )}
                     </Mutation>
-                  ) : (
-                    <div>-</div>
-                  )}
-                  <div>{data.route.attempts}</div>
-                  <Mutation
-                    mutation={UPDATE_ATTEMPT_MUTATION}
-                    variables={{
-                      id: data.route.id,
-                      attempts: data.route.attempts + 1
-                    }}
-                  >
-                    {mutation => <div onClick={mutation}>+</div>}
-                  </Mutation>
-                </div>
-                <div>
-                  <h3>
-                    SIGNATURES:{" "}
-                    {data.route.signatures ? data.route.signatures.length : 0}
-                  </h3>
-                  <div />
-                  <div />
-                </div>
-                <Button className='submit-button'>SUBMIT</Button>
-              </div>
+                  </CounterContainer>
+                </AttemptsContainer>
+                <Signatures>
+                  <H3>SIGNATURES: </H3>
+                  {!data.route.signatures ? (
+                    <CheckMarksContainer>
+                      <IoMdRadioButtonOff size={"50px"} />
+                      <IoMdRadioButtonOff size={"50px"} />
+                    </CheckMarksContainer>
+                  ) : null}
+                  {data.route.signatures &&
+                  data.route.signatures.length === 1 ? (
+                    <CheckMarksContainer>
+                      <IoMdCheckmarkCircle size={"50px"} />
+                      <IoMdRadioButtonOff size={"50px"} />
+                    </CheckMarksContainer>
+                  ) : null}
+                  {data.route.signatures &&
+                  data.route.signatures.length === 1 ? (
+                    <CheckMarksContainer>
+                      <IoMdCheckmarkCircle size={"50px"} />
+                      <IoMdCheckmarkCircle size={"50px"} />
+                    </CheckMarksContainer>
+                  ) : null}
+                </Signatures>
+                <ButtonContainer>
+                  <Button>SUBMIT</Button>
+                </ButtonContainer>
+              </InnerCardWrapper>
             );
           }}
         </Query>
@@ -101,10 +134,73 @@ const Container = styled.div`
 `;
 
 const Card = styled.div`
-  width: 90%;
+  width: 85%;
   height: 80%;
   background: #dfe2e8;
   display: flex;
   flex-direction: column;
   align-items: center;
+  border-radius: 10px;
+  justify-content: space-between;
+  padding: 10px;
+  margin-top: 20px;
 `;
+
+const AttemptsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  width: 80%;
+`;
+
+const CounterContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+`;
+
+const InnerCardWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding-bottom: 50px;
+`;
+
+const Attempts = styled.p`
+  font-size: 45px;
+  font-weight: 500;
+`;
+
+const Signatures = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CheckMarksContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0;
+`;
+
+const H3 = styled.h3`
+  height: 20px;
+  margin: 0;
+`;
+
+const ButtonContainer = styled.div`
+  width: 75%;
+`;
+
+const Back = styled.div``;
