98ab803e2e6f1d58aa27a51d27218e0cff949aa8
Joey Ricketts <joeyricketts@spires-MacBook-Pro.local>
2019-08-20 11:10:07 -0600

    fixed server http endpoint for prod

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { setContext } from 'apollo-link-context';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { AUTH_TOKEN } from './constants';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { setContext } from "apollo-link-context";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { AUTH_TOKEN } from "./constants";
 
 // connecting to the api
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: "https://compcard-3fbed8df85.herokuapp.com/default/default"
 });
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN);
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
+      authorization: token ? `Bearer ${token}` : ""
     }
   };
 });
@@ -34,7 +34,7 @@ ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
